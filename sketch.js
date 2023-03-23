825efd8710291d604f32d339f08abcf452d9f44e
paaki123 <128678369+paaki123@users.noreply.github.com>
2023-03-23 12:41:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var box
+var box1
+function setup() {
+  createCanvas(1000,1000);
+  box=createSprite(200,200,30,30)
+  box1=createSprite(600,600,30,30)
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites()
+
+}
+
+
+
+
